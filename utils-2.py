1b7b1b62d0c0656df9e81b2c8e47f011b08bdb82
Olve Drageset <olve.drageset@gmail.no>
2022-02-22 14:42:14 +0100

    guardian, gcs, and pandas functions

--- utils-2.py
+++ utils-2.py
@@ -1,8 +1,58 @@
+import json
 import os
 import requests
-from tornado import web
-from jupyterhub.handlers import BaseHandler
+import pandas as pd
 from jupyterhub.services.auth import HubAuth
+from google.oauth2.credentials import Credentials
+
+
+def call_endpoint(api_endpoint, maskinporten_client_id, scopes, guardian_endpoint="https://guardian.dapla.ssb.no/maskinporten/access-token", keycloak_token=""):
+    """
+    Call an external API using the maskinporten guardian
+    :param api_endpoint: URL to the target API
+    :param maskinporten_client_id: the Maskinporten client id
+    :param scopes: the Maskinporten scopes
+    :param guardian_endpoint: URL to the Maskinporten Guardian
+    :param keycloak_token: the user's personal Keycloak token. Automatic fetch attempt will be made if left empty.
+    :return: the endpoint json response
+    """
+    if keycloak_token == "":
+        keycloak_token = AuthClient.fetch_personal_token()
+    body = {
+        "maskinportenClientId": maskinporten_client_id,
+        "scopes": scopes  # Array
+    }
+    body = json.dumps(body)
+    maskinporten_token = get_guardian_token(guardian_endpoint, keycloak_token, body)
+    api_response = requests.get(api_endpoint,
+                                headers={
+                                    'Authorization': 'Bearer %s' % maskinporten_token,
+                                    'Accept': 'application/json'
+                                })
+    if api_response.status_code == 200:
+        return api_response.json()
+    else:
+        raise RuntimeError(f'Error calling target endpoint: <{api_response.status_code}: {api_response.text or api_response.reason}>')
+
+
+def get_guardian_token(guardian_endpoint, keycloak_token, body):
+    """
+    Retrieve access token from makinporten guardian
+    :param guardian_endpoint: URL to the maskinporten guardian
+    :param keycloak_token: the user's Keycloak token
+    :param body: maskinporten request body
+    :return: the maskinporten access token
+    """
+    guardian_response = requests.post(guardian_endpoint,
+                                      headers={
+                                          'Authorization': 'Bearer %s' % keycloak_token,
+                                          'Content-type': 'application/json'
+                                      }, data=body)
+    if guardian_response.status_code == 200:
+        return guardian_response.json()['accessToken']
+    else:
+        raise RuntimeError(f'Error getting guardian token: <{guardian_response.status_code}: {guardian_response.text or guardian_response.reason}>')
+
 
 def get_gcs_file_system():
     """
@@ -10,14 +60,35 @@ def get_gcs_file_system():
     See https://gcsfs.readthedocs.io/en/latest for usage
     """
     import gcsfs
-    from google.oauth2.credentials import Credentials
+    token = get_gcs_credentials()
+    return gcsfs.GCSFileSystem(token=token)
 
-    token = Credentials(
+
+def get_gcs_credentials():
+    return Credentials(
         token=AuthClient.fetch_google_token(),
         token_uri="https://oauth2.googleapis.com/token",
     )
 
-    return gcsfs.GCSFileSystem(token=token)
+
+def gcs_ls(bucket_name):
+    fs = get_gcs_file_system()
+    return fs.ls(bucket_name)
+
+
+def pandas_read_csv(path):
+    df = pd.read_csv(f"gcs://{path}", storage_options={"token": get_gcs_credentials()})
+    return df
+
+
+def pandas_read_json(path):
+    df = pd.read_json(f"gcs://{path}", storage_options={"token": get_gcs_credentials()})
+    return df
+
+
+def pandas_read_xml(path):
+    df = pd.read_xml(f"gcs://{path}", storage_options={"token": get_gcs_credentials()})
+    return df
 
 
 class AuthClient:
@@ -44,14 +115,12 @@ class AuthClient:
         except AuthError as err:
             err.print_warning()
 
-
     @staticmethod
     def fetch_google_token():
         try:
             return AuthClient.fetch_local_user()['exchanged_tokens']['google']['access_token']
         except AuthError as err:
             err.print_warning()
- 
 
     @staticmethod
     def is_ready():
@@ -65,3 +134,4 @@ class AuthError(Exception):
     def print_warning(self):
         from IPython.core.display import display, HTML
         display(HTML('Your session has timed out. Please <a href="/hub/login">log in</a> to continue.'))
+
