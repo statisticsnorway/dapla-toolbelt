188b010bef8434c716c72f016f862233f277699f
Olve Drageset <olve.drageset@gmail.no>
2022-02-24 14:34:03 +0100

    v0.0.2 fix

--- setup.py
+++ setup.py
@@ -9,8 +9,8 @@ README = (HERE / "README.md").read_text()
 
 # This call to setup() does all the work
 setup(
-    name="dapla-toolbelt",
-    version="0.0.1",
+    name="dapla_toolbelt",
+    version="0.0.2",
     description="Authorized read and write against GCS when logged in with Keycloak in Jupyter",
     long_description=README,
     long_description_content_type="text/markdown",
@@ -21,7 +21,7 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.9",
     ],
-    packages=["dapla-toolbelt"],
+    packages=["dapla_toolbelt"],
     include_package_data=True,
     install_requires=["pandas", "jupyterhub", "gcsfs", "ipython"],
 )
