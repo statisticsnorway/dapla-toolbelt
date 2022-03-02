1cab5ce45ea5756e434ca7b21363e29bc26497e2
Olve Drageset <olve.drageset@gmail.no>
2022-03-02 11:12:33 +0100

    fix local imports

--- dapla_toolbelt/files.py
+++ dapla_toolbelt/files.py
@@ -1,118 +1,121 @@
 import pandas as pd
-from auth import AuthClient
 import gcsfs
 
-
-def ensure_gcs_uri_prefix(gcs_path):
-    """
-    GCS uri is the gcs file path prefixed with 'gs://'. Some operations require GCS uris,
-    but we don't want the user to bother with knowing when to use the prefix,
-    so we ensure its presence automatically where it is needed
-    :param gcs_path:
-    :return:
-    """
-    gs_uri_prefix = "gs://"
-    if not gcs_path.startswith(gs_uri_prefix):
-        gcs_path = f"{gs_uri_prefix}{gcs_path}"
-    return gcs_path
+from .auth import AuthClient
 
 
-def get_gcs_file_system():
-    """
-    Return a pythonic file-system for Google Cloud Storage - initialized with a personal Google Identity token.
-    See https://gcsfs.readthedocs.io/en/latest for usage
-    """
-    return gcsfs.GCSFileSystem(token=AuthClient.fetch_google_credentials())
+class FileClient:
+    @staticmethod
+    def ensure_gcs_uri_prefix(gcs_path):
+        """
+        GCS uri is the gcs file path prefixed with 'gs://'. Some operations require GCS uris,
+        but we don't want the user to bother with knowing when to use the prefix,
+        so we ensure its presence automatically where it is needed
+        :param gcs_path:
+        :return:
+        """
+        gs_uri_prefix = "gs://"
+        if not gcs_path.startswith(gs_uri_prefix):
+            gcs_path = f"{gs_uri_prefix}{gcs_path}"
+        return gcs_path
 
+    @staticmethod
+    def get_gcs_file_system():
+        """
+        Return a pythonic file-system for Google Cloud Storage - initialized with a personal Google Identity token.
+        See https://gcsfs.readthedocs.io/en/latest for usage
+        """
+        return gcsfs.GCSFileSystem(token=AuthClient.fetch_google_credentials())
 
-def ls(bucket_name):
-    """
-    List the contents of a GCS bucket
-    :param bucket_name:
-    :return: Array of contents of bucket
-    """
-    fs = get_gcs_file_system()
-    return fs.ls(bucket_name)
+    @staticmethod
+    def ls(bucket_name):
+        """
+        List the contents of a GCS bucket
+        :param bucket_name:
+        :return: Array of contents of bucket
+        """
+        fs = FileClient.get_gcs_file_system()
+        return fs.ls(bucket_name)
 
+    @staticmethod
+    def cat(gcs_path):
+        """
+        Get string content of file from gcs
+        :param gcs_path: path or paths to the file(s) you want to get the contents of
+        :return: utf-8 decoded string content of the given file
+        """
+        fs = FileClient.get_gcs_file_system()
+        return fs.cat(gcs_path).decode("utf-8")
 
-def cat(gcs_path):
-    """
-    Get string content of file from gcs
-    :param gcs_path: path or paths to the file(s) you want to get the contents of
-    :return: utf-8 decoded string content of the given file
-    """
-    fs = get_gcs_file_system()
-    return fs.cat(gcs_path).decode("utf-8")
+    @staticmethod
+    def gcs_open(gcs_path, mode='r'):
+        """
+        Open a file in GCS, works like regular python open()
+        :param gcs_path:
+        :param mode:
+        :return:
+        """
+        FileClient.get_gcs_file_system().open(FileClient.ensure_gcs_uri_prefix(gcs_path), mode)
 
+    @staticmethod
+    def load_csv_to_pandas(gcs_path):
+        """
+        Reads a csv file from google cloud storage into a Pandas data frame
+        :param gcs_path: of the file, starting with the bucket name
+        :return: a Pandas data frame
+        """
+        df = pd.read_csv(FileClient.ensure_gcs_uri_prefix(gcs_path), storage_options={"token": AuthClient.fetch_google_credentials()})
+        return df
 
-def gcs_open(gcs_path, mode='r'):
-    """
-    Open a file in GCS, works like regular python open()
-    :param gcs_path:
-    :param mode:
-    :return:
-    """
-    get_gcs_file_system().open(ensure_gcs_uri_prefix(gcs_path), mode)
+    @staticmethod
+    def load_json_to_pandas(gcs_path):
+        """
+        Reads a json file from google cloud storage into a Pandas data frame
+        :param gcs_path: of the file, starting with the bucket name
+        :return: a Pandas data frame
+        """
+        df = pd.read_json(FileClient.ensure_gcs_uri_prefix(gcs_path), storage_options={"token": AuthClient.fetch_google_credentials()})
+        return df
 
+    @staticmethod
+    def load_xml_to_pandas(gcs_path):
+        """
+        Reads an xml file from google cloud storage into a Pandas data frame
+        :param gcs_path: of the file, starting with the bucket name
+        :return: a Pandas data frame
+        """
+        df = pd.read_xml(FileClient.ensure_gcs_uri_prefix(gcs_path), storage_options={"token": AuthClient.fetch_google_credentials()})
+        return df
 
-def load_csv_to_pandas(gcs_path):
-    """
-    Reads a csv file from google cloud storage into a Pandas data frame
-    :param gcs_path: of the file, starting with the bucket name
-    :return: a Pandas data frame
-    """
-    df = pd.read_csv(ensure_gcs_uri_prefix(gcs_path), storage_options={"token": AuthClient.fetch_google_credentials()})
-    return df
+    @staticmethod
+    def save_pandas_to_csv(df: pd.DataFrame, gcs_path, index=False):
+        """
+        Write the contents of a Pandas data frame to a csv file in a bucket
+        :param df: the Pandas data frame to persist as csv
+        :param gcs_path: target path, starting with the bucket name and ending with the file name
+        :param index: True if you want to write the pandas index to the file
+        :return:
+        """
+        df.to_csv(FileClient.ensure_gcs_uri_prefix(gcs_path), storage_options={"token": AuthClient.fetch_google_credentials()}, index=index)
 
+    @staticmethod
+    def save_pandas_to_json(df: pd.DataFrame, gcs_path, index=False):
+        """
+        Write the contents of a Pandas data frame to a json file in a bucket
+        :param df: the Pandas data frame to persist as json
+        :param gcs_path: target path, starting with the bucket name and ending with the file name
+        :param index: True if you want to write the pandas index to the file
+        :return:
+        """
+        df.to_json(FileClient.ensure_gcs_uri_prefix(gcs_path), storage_options={"token": AuthClient.fetch_google_credentials()}, index=index)
 
-def load_json_to_pandas(gcs_path):
-    """
-    Reads a json file from google cloud storage into a Pandas data frame
-    :param gcs_path: of the file, starting with the bucket name
-    :return: a Pandas data frame
-    """
-    df = pd.read_json(ensure_gcs_uri_prefix(gcs_path), storage_options={"token": AuthClient.fetch_google_credentials()})
-    return df
-
-
-def load_xml_to_pandas(gcs_path):
-    """
-    Reads an xml file from google cloud storage into a Pandas data frame
-    :param gcs_path: of the file, starting with the bucket name
-    :return: a Pandas data frame
-    """
-    df = pd.read_xml(ensure_gcs_uri_prefix(gcs_path), storage_options={"token": AuthClient.fetch_google_credentials()})
-    return df
-
-
-def save_pandas_to_csv(df: pd.DataFrame, gcs_path, index=False):
-    """
-    Write the contents of a Pandas data frame to a csv file in a bucket
-    :param df: the Pandas data frame to persist as csv
-    :param gcs_path: target path, starting with the bucket name and ending with the file name
-    :param index: True if you want to write the pandas index to the file
-    :return:
-    """
-    df.to_csv(ensure_gcs_uri_prefix(gcs_path), storage_options={"token": AuthClient.fetch_google_credentials()}, index=index)
-
-
-def save_pandas_to_json(df: pd.DataFrame, gcs_path, index=False):
-    """
-    Write the contents of a Pandas data frame to a json file in a bucket
-    :param df: the Pandas data frame to persist as json
-    :param gcs_path: target path, starting with the bucket name and ending with the file name
-    :param index: True if you want to write the pandas index to the file
-    :return:
-    """
-    df.to_json(ensure_gcs_uri_prefix(gcs_path), storage_options={"token": AuthClient.fetch_google_credentials()}, index=index)
-
-
-def save_pandas_to_xml(df: pd.DataFrame, gcs_path, index=False):
-    """
-    Write the contents of a Pandas data frame to an xml file in a bucket
-    :param df: the Pandas data frame to persist as xml
-    :param gcs_path: target path, starting with the bucket name and ending with the file name
-    :param index: True if you want to write the pandas index to the file
-    :return:
-    """
-    df.to_xml(ensure_gcs_uri_prefix(gcs_path), storage_options={"token": AuthClient.fetch_google_credentials()}, index=index)
+    @staticmethod
+    def save_pandas_to_xml(df: pd.DataFrame, gcs_path, index=False):
+        """
+        Write the contents of a Pandas data frame to an xml file in a bucket
+        :param df: the Pandas data frame to persist as xml
+        :param gcs_path: target path, starting with the bucket name and ending with the file name
+        :param index: True if you want to write the pandas index to the file
+        :return:
+        """
+        df.to_xml(FileClient.ensure_gcs_uri_prefix(gcs_path), storage_options={"token": AuthClient.fetch_google_credentials()}, index=index)
--- dapla_toolbelt/guardian.py
+++ dapla_toolbelt/guardian.py
@@ -1,52 +1,55 @@
 import json
 import requests
-from auth import AuthClient
+
+from .auth import AuthClient
 
 
-def call_endpoint(api_endpoint, maskinporten_client_id, scopes, guardian_endpoint="https://guardian.dapla.ssb.no/maskinporten/access-token", keycloak_token=""):
-    """
-    Call an external API using the maskinporten guardian
-    :param api_endpoint: URL to the target API
-    :param maskinporten_client_id: the Maskinporten client id
-    :param scopes: the Maskinporten scopes
-    :param guardian_endpoint: URL to the Maskinporten Guardian
-    :param keycloak_token: the user's personal Keycloak token. Automatic fetch attempt will be made if left empty.
-    :return: the endpoint json response
-    """
-    if keycloak_token == "":
-        keycloak_token = AuthClient.fetch_personal_token()
-    body = {
-        "maskinportenClientId": maskinporten_client_id,
-        "scopes": scopes  # Array
-    }
-    body = json.dumps(body)
-    maskinporten_token = get_guardian_token(guardian_endpoint, keycloak_token, body)
-    api_response = requests.get(api_endpoint,
-                                headers={
-                                    'Authorization': 'Bearer %s' % maskinporten_token,
-                                    'Accept': 'application/json'
-                                })
-    if api_response.status_code == 200:
-        return api_response.json()
-    else:
-        raise RuntimeError(f'Error calling target endpoint: <{api_response.status_code}: {api_response.text or api_response.reason}>')
+class GuardianClient:
+    @staticmethod
+    def call_api(api_endpoint_url, maskinporten_client_id, scopes, guardian_endpoint_url="https://guardian.dapla.ssb.no/maskinporten/access-token", keycloak_token=""):
+        """
+        Call an external API using the maskinporten guardian
+        :param api_endpoint_url: URL to the target API
+        :param maskinporten_client_id: the Maskinporten client id
+        :param scopes: the Maskinporten scopes
+        :param guardian_endpoint_url: URL to the Maskinporten Guardian
+        :param keycloak_token: the user's personal Keycloak token. Automatic fetch attempt will be made if left empty.
+        :return: the endpoint json response
+        """
+        if keycloak_token == "":
+            keycloak_token = AuthClient.fetch_personal_token()
+        body = {
+            "maskinportenClientId": maskinporten_client_id,
+            "scopes": scopes  # Array
+        }
+        body = json.dumps(body)
+        maskinporten_token = GuardianClient.get_guardian_token(guardian_endpoint_url, keycloak_token, body)
+        api_response = requests.get(api_endpoint_url,
+                                    headers={
+                                        'Authorization': 'Bearer %s' % maskinporten_token,
+                                        'Accept': 'application/json'
+                                    })
+        if api_response.status_code == 200:
+            return api_response.json()
+        else:
+            raise RuntimeError(f'Error calling target endpoint: <{api_response.status_code}: {api_response.text or api_response.reason}>')
 
-
-def get_guardian_token(guardian_endpoint, keycloak_token, body):
-    """
-    Retrieve access token from makinporten guardian
-    :param guardian_endpoint: URL to the maskinporten guardian
-    :param keycloak_token: the user's Keycloak token
-    :param body: maskinporten request body
-    :return: the maskinporten access token
-    """
-    guardian_response = requests.post(guardian_endpoint,
-                                      headers={
-                                          'Authorization': 'Bearer %s' % keycloak_token,
-                                          'Content-type': 'application/json'
-                                      }, data=body)
-    if guardian_response.status_code == 200:
-        return guardian_response.json()['accessToken']
-    else:
-        raise RuntimeError(f'Error getting guardian token: <{guardian_response.status_code}: {guardian_response.text or guardian_response.reason}>')
+    @staticmethod
+    def get_guardian_token(guardian_endpoint, keycloak_token, body):
+        """
+        Retrieve access token from makinporten guardian
+        :param guardian_endpoint: URL to the maskinporten guardian
+        :param keycloak_token: the user's Keycloak token
+        :param body: maskinporten request body
+        :return: the maskinporten access token
+        """
+        guardian_response = requests.post(guardian_endpoint,
+                                          headers={
+                                              'Authorization': 'Bearer %s' % keycloak_token,
+                                              'Content-type': 'application/json'
+                                          }, data=body)
+        if guardian_response.status_code == 200:
+            return guardian_response.json()['accessToken']
+        else:
+            raise RuntimeError(f'Error getting guardian token: <{guardian_response.status_code}: {guardian_response.text or guardian_response.reason}>')
 
