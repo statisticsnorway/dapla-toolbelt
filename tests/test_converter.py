bde380ee81abe6c3bc5997e805b26b9e4594a3f0
Kenneth Leine Schulstad <ks@udp.no>
2022-05-02 15:19:59 +0200

    Add converter funcs: start_simulation, get_pseudo_report & get_pseudo_schema

--- tests/test_converter.py
+++ tests/test_converter.py
@@ -34,6 +34,79 @@ sample_response_get_job_summary = '''
 }
 '''
 
+sample_response_pseudo_report = '''
+{
+  "matchedPseudoRules": [
+    {
+      "name": "personidentifikator",
+      "pattern": "**/personidentifikator",
+      "func": "fpe-fnr(testsecret1)"
+    },
+    {
+      "name": "navn",
+      "pattern": "**/navn",
+      "func": "fpe-anychar(testsecret1)"
+    }
+  ],
+  "unmatchedPseudoRules": [
+    {
+      "name": "adresse",
+      "pattern": "**/{adresse,gateadresse}",
+      "func": "fpe-anychar(testsecret1)"
+    }
+  ],
+  "ruleToFieldMatches": {
+    "navn": [
+      "/root/foo/bar/navn",
+      "/root/foo/bar/baz/navn"
+    ],
+    "personidentifikator": [
+      "/root/foo/personidentifikator"
+    ]
+  },
+  "fieldToRuleMatches": {
+    "/root/foo/bar/navn": "navn",
+    "/root/foo/bar/baz/navn": "navn",
+    "/root/foo/personidentifikator": "personidentifikator"
+  },
+  "targetSchemaHierachy": "<removed for brevity>",
+  "metrics": {
+    "totalPseudoRulesCount": 3,
+    "totalFieldsCount": 3034,
+    "unmatchedPseudoRulesCount": 1,
+    "pseudonymizedFieldsCount": 3,
+    "matchedPseudoRulesCount": 2
+  }
+}
+'''
+
+sample_response_pseudo_schema = '''
+root record required
+ |-- metadata record
+ |    |-- collector record
+ |    |    |-- ulid string required
+ |    |    |-- timestamp long required
+ |    |    |-- position string required
+ |    |-- converter record
+ |    |    |-- jobId string required
+ |    |    |-- timestamp long required
+ |    |    |-- coreVersion string required
+ |    |    |-- props map
+ |    |    |-- pseudo record
+ |    |    |    |-- rules array
+ |    |    |    |    |-- rules record
+ |    |    |    |    |    |-- name string
+ |    |    |    |    |    |-- pattern string required
+ |    |    |    |    |    |-- func string required
+ |-- foo record
+ |    |-- personidentifikator string required pseudo:fpe-fnr(testsecret1)
+ |    |-- bar record
+ |    |    |-- navn string required pseudo:fpe-anychar(testsecret1)
+ |    |    |-- baz record
+ |    |    |    |-- navn string required pseudo:fpe-anychar(testsecret1)
+ |    |-- opprettetDato string
+'''
+
 
 @mock.patch('dapla.auth.AuthClient')
 @responses.activate
@@ -43,6 +116,19 @@ def test_converter_start_200_response(auth_client_mock):
     responses.add(responses.POST, 'https://mock-converter.no/jobs', json=sample_response_start_job, status=200)
     client = ConverterClient(converter_test_url)
     response = client.start(job_config)
+    json_str = json.loads(response.json())
+
+    assert json_str['jobId'] == json.loads(sample_response_start_job)['jobId']
+
+
+@mock.patch('dapla.auth.AuthClient')
+@responses.activate
+def test_converter_start_simulation_200_response(auth_client_mock):
+    auth_client_mock.fetch_personal_token.return_value = fake_token
+    job_config = {}
+    responses.add(responses.POST, 'https://mock-converter.no/jobs/simulation', json=sample_response_start_job, status=200)
+    client = ConverterClient(converter_test_url)
+    response = client.start_simulation(job_config)
     json_str = json.loads(response.json())
 
     assert json_str['jobId'] == json.loads(sample_response_start_job)['jobId']
@@ -68,3 +154,25 @@ def test_converter_stop_job_200_response(auth_client_mock):
     response = client.stop_job(json.loads(sample_response_start_job)['jobId'])
 
     assert response.status_code == 200
+
+
+@mock.patch('dapla.auth.AuthClient')
+@responses.activate
+def test_converter_get_pseudo_report_200_response(auth_client_mock):
+    auth_client_mock.fetch_personal_token.return_value = fake_token
+    responses.add(responses.GET, 'https://mock-converter.no/jobs/01FZWP8R3PHDYD5QQS4CY1RKBW/reports/pseudo', json=sample_response_pseudo_report, status=200)
+    client = ConverterClient(converter_test_url)
+    response = client.get_pseudo_report(json.loads(sample_response_start_job)['jobId'])
+
+    assert json.loads(response.json()) == json.loads(sample_response_pseudo_report)
+
+
+@mock.patch('dapla.auth.AuthClient')
+@responses.activate
+def test_converter_get_pseudo_schema_200_response(auth_client_mock):
+    auth_client_mock.fetch_personal_token.return_value = fake_token
+    responses.add(responses.GET, 'https://mock-converter.no/jobs/01FZWP8R3PHDYD5QQS4CY1RKBW/reports/pseudo-schema-hierarchy', json=sample_response_pseudo_schema, status=200)
+    client = ConverterClient(converter_test_url)
+    response = client.get_pseudo_schema(json.loads(sample_response_start_job)['jobId'])
+
+    assert len(response.text) > 0
