6156f3254a98739589770c41a694ec0ef93808a4
Skaar, Bjørn-Andre <bjorn.skaar@ssb.no>
2022-09-06 11:21:01 +0200

    Fixed imports

--- dapla/__init__.py
+++ dapla/__init__.py
@@ -5,5 +5,6 @@ from .files import FileClient
 from .guardian import GuardianClient
 from .collector import CollectorClient
 from .converter import ConverterClient
-from .backports import show, details, read_pandas, write_pandas
+from .backports import show, details
+from .pandas import read_pandas, write_pandas
 from .jupyterhub import generate_api_token
