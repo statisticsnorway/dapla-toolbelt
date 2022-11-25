911644a72f67333c4f9c75ca7be2f78216961ba4
Skaar, Bjørn-Andre <bjorn.skaar@ssb.no>
2022-11-25 14:02:02 +0100

    Fix tests for gcs

---
+++ tests/test_gcs.py
@@ -0,0 +1,8 @@
+from dapla.gcs import GCSFileSystem
+
+
+def test_instance():
+    # Chack that instantiation works with the current version of pyarrow
+    client = GCSFileSystem()
+    assert client is not None
+
