b593f0396c348906c6d0bd60bc40215478a4e5c1
Anders Lunde <anders-lunde@hotmail.com>
2023-01-13 12:16:53 +0100

    fix precommit issues

--- dapla/__init__.py
+++ dapla/__init__.py
@@ -1,11 +1,1 @@
-__version__ = "1.3.7"
-
-from .auth import AuthClient
-from .files import FileClient
-from .doctor import Doctor
-from .guardian import GuardianClient
-from .collector import CollectorClient
-from .converter import ConverterClient
-from .backports import show, details
-from .pandas import read_pandas, write_pandas
-from .jupyterhub import generate_api_token
+__version__ = "1.3.8"
