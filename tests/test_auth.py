52a3eb001c01ba35aefe6c5e79b7b734ce0963e1
Skaar, Bjørn-Andre <bjorn.skaar@ssb.no>
2023-01-16 22:42:26 +0100

    Fix test

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -99,5 +99,5 @@ def test_fetch_google_credentials():
     response.refresh(None)
 
     assert response.token == 'google_token'
-    assert not response.expired()
+    assert response.expired() == False
     assert len(responses.calls) == 2
