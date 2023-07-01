eb952d65809ff37cd0ed62bddf6cae91e6fe3e21
Arne Sørli <81353974+arneso-ssb@users.noreply.github.com>
2023-07-01 07:46:32 +0200

    Add new function repo_root_dir
    - For reading config files and other files in a uniform way, example:
      config_file = dp.repo_root_dir() / "config" / "config.toml"
    - Update dependencies

---
+++ tests/test_git.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+
+import pytest
+
+from dapla.git import repo_root_dir
+
+
+def test_repo_root_dir():
+    # Directory in a git repo
+    repo_root = repo_root_dir()
+    assert repo_root is not None
+
+    # Directory outside a git repo
+    with pytest.raises(RuntimeError):
+        repo_root_dir(Path("/"))
+
+    # Directory as string instead of pathlib.Path
+    dir_string = str(Path.cwd())
+    repo_root_str = repo_root_dir(dir_string)
+    assert repo_root_str is not None
