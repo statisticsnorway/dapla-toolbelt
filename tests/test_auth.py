f998ef319ed59ca5158652e61d35a53f979ae9d5
Anders Lunde <8294494+Andilun@users.noreply.github.com>
2023-09-26 12:49:24 +0200

    Update test with new response and request type

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,39 +1,46 @@
-import mock
+from datetime import datetime, timedelta
+from unittest import mock
+
 import responses
-from datetime import datetime, timedelta, timezone
 
 from dapla.auth import AuthClient
 
-auth_endpoint_url = 'https://mock-auth.no/user'
+auth_endpoint_url = "https://mock-auth.no/user"
 
 
-@mock.patch.dict('dapla.auth.os.environ', {'LOCAL_USER_PATH': auth_endpoint_url}, clear=True)
+@mock.patch.dict(
+    "dapla.auth.os.environ", {"LOCAL_USER_PATH": auth_endpoint_url}, clear=True
+)
 def test_is_ready():
     client = AuthClient()
     assert client.is_ready()
 
 
-@mock.patch.dict('dapla.auth.os.environ', {'LOCAL_USER_PATH': auth_endpoint_url}, clear=True)
+@mock.patch.dict(
+    "dapla.auth.os.environ", {"LOCAL_USER_PATH": auth_endpoint_url}, clear=True
+)
 @responses.activate
 def test_fetch_personal_token():
     mock_response = {
-        'access_token': 'fake_access_token',
+        "access_token": "fake_access_token",
     }
     responses.add(responses.GET, auth_endpoint_url, json=mock_response, status=200)
 
     client = AuthClient()
     response = client.fetch_personal_token()
 
-    assert response == 'fake_access_token'
+    assert response == "fake_access_token"
     assert len(responses.calls) == 1
 
 
-@mock.patch.dict('dapla.auth.os.environ', {'LOCAL_USER_PATH': auth_endpoint_url}, clear=True)
-@mock.patch('dapla.auth.display')
+@mock.patch.dict(
+    "dapla.auth.os.environ", {"LOCAL_USER_PATH": auth_endpoint_url}, clear=True
+)
+@mock.patch("dapla.auth.display")
 @responses.activate
 def test_fetch_personal_token_error(mock_display):
     mock_response = {
-        'message': 'There was an error',
+        "message": "There was an error",
     }
     responses.add(responses.GET, auth_endpoint_url, json=mock_response, status=404)
 
@@ -43,13 +50,15 @@ def test_fetch_personal_token_error(mock_display):
     mock_display.assert_called_once()
 
 
-@mock.patch.dict('dapla.auth.os.environ', {'LOCAL_USER_PATH': auth_endpoint_url}, clear=True)
-@mock.patch('dapla.auth.display')
+@mock.patch.dict(
+    "dapla.auth.os.environ", {"LOCAL_USER_PATH": auth_endpoint_url}, clear=True
+)
+@mock.patch("dapla.auth.display")
 @responses.activate
 def test_fetch_google_token_error(mock_display):
     mock_display
     mock_response = {
-        'message': 'There was an error',
+        "message": "There was an error",
     }
     responses.add(responses.GET, auth_endpoint_url, json=mock_response, status=404)
 
@@ -59,56 +68,62 @@ def test_fetch_google_token_error(mock_display):
     mock_display.assert_called_once()
 
 
-@mock.patch.dict('dapla.auth.os.environ', {'LOCAL_USER_PATH': auth_endpoint_url}, clear=True)
+@mock.patch.dict(
+    "dapla.auth.os.environ", {"LOCAL_USER_PATH": auth_endpoint_url}, clear=True
+)
 @responses.activate
-def test_fetch_google_token():
+def test_fetch_google_token_dapla_jupyter():
     mock_response = {
-        'access_token': 'fake_access_token',
-        'exchanged_tokens': {
-            'google': {
-                'access_token': 'google_token',
-                'exp': round((datetime.now() + timedelta(hours=1)).timestamp())
+        "access_token": "fake_access_token",
+        "exchanged_tokens": {
+            "google": {
+                "access_token": "google_token",
+                "exp": round((datetime.now() + timedelta(hours=1)).timestamp()),
             }
-        }
+        },
     }
     responses.add(responses.GET, auth_endpoint_url, json=mock_response, status=200)
 
     client = AuthClient()
     response = client.fetch_google_token()
 
-    assert response == 'google_token'
+    assert response == "google_token"
     assert len(responses.calls) == 1
 
 
-@mock.patch.dict('dapla.auth.os.environ', {
-    'OIDC_TOKEN_EXCHANGE_URL': auth_endpoint_url,
-    'OIDC_TOKEN': 'fake_access_token'}, clear=True)
+@mock.patch.dict(
+    "dapla.auth.os.environ",
+    {"OIDC_TOKEN_EXCHANGE_URL": auth_endpoint_url, "OIDC_TOKEN": "fake_access_token"},
+    clear=True,
+)
 @responses.activate
-def test_fetch_google_token():
+def test_fetch_google_token_dapla_lab():
     mock_response = {
-        'access_token': 'google_token',
-        'accessTokenExpiration': round((datetime.now() + timedelta(hours=1)).timestamp())
+        "access_token": "google_token",
+        "expires_in": round((datetime.now() + timedelta(hours=1)).timestamp()),
     }
-    responses.add(responses.GET, auth_endpoint_url, json=mock_response, status=200)
+    responses.add(responses.POST, auth_endpoint_url, json=mock_response, status=200)
 
     client = AuthClient()
     response = client.fetch_google_token()
 
-    assert response == 'google_token'
+    assert response == "google_token"
     assert len(responses.calls) == 1
 
 
-@mock.patch.dict('dapla.auth.os.environ', {'LOCAL_USER_PATH': auth_endpoint_url}, clear=True)
+@mock.patch.dict(
+    "dapla.auth.os.environ", {"LOCAL_USER_PATH": auth_endpoint_url}, clear=True
+)
 @responses.activate
 def test_fetch_google_credentials():
     mock_response = {
-        'access_token': 'fake_access_token',
-        'exchanged_tokens': {
-            'google': {
-                'access_token': 'google_token',
-                'exp': round((datetime.now() + timedelta(hours=1)).timestamp())
+        "access_token": "fake_access_token",
+        "exchanged_tokens": {
+            "google": {
+                "access_token": "google_token",
+                "exp": round((datetime.now() + timedelta(hours=1)).timestamp()),
             }
-        }
+        },
     }
     responses.add(responses.GET, auth_endpoint_url, json=mock_response, status=200)
 
@@ -116,26 +131,29 @@ def test_fetch_google_credentials():
     response = client.fetch_google_credentials()
     response.refresh(None)
 
-    assert response.token == 'google_token'
+    assert response.token == "google_token"
     assert len(responses.calls) == 2
     assert not response.expired
 
-@mock.patch.dict('dapla.auth.os.environ', {'LOCAL_USER_PATH': auth_endpoint_url}, clear=True)
+
+@mock.patch.dict(
+    "dapla.auth.os.environ", {"LOCAL_USER_PATH": auth_endpoint_url}, clear=True
+)
 @responses.activate
 def test_fetch_google_credentials_expired():
     mock_response = {
-        'access_token': 'fake_access_token',
-        'exchanged_tokens': {
-            'google': {
-                'access_token': 'google_token',
-                'exp': round((datetime.now() - timedelta(hours=1)).timestamp())
+        "access_token": "fake_access_token",
+        "exchanged_tokens": {
+            "google": {
+                "access_token": "google_token",
+                "exp": round((datetime.now() - timedelta(hours=1)).timestamp()),
             }
-        }
+        },
     }
     responses.add(responses.GET, auth_endpoint_url, json=mock_response, status=200)
 
     # Add 2 hours to expiry timestamp for refresh response
-    mock_response['exchanged_tokens']['google']['exp'] += 7200
+    mock_response["exchanged_tokens"]["google"]["exp"] += 7200
     responses.add(responses.GET, auth_endpoint_url, json=mock_response, status=200)
 
     client = AuthClient()
@@ -145,4 +163,3 @@ def test_fetch_google_credentials_expired():
 
     response.refresh(None)
     assert not response.expired
-
