c2c7e65be696385b51745e42caf350a58ee25c36
Andilun <8294494+Andilun@users.noreply.github.com>
2024-03-22 12:52:02 +0100

    Add test that confirms that there is no deadlock on credentials refresh

--- tests/test_gcs.py
+++ tests/test_gcs.py
@@ -1,3 +1,11 @@
+from datetime import timedelta
+from unittest.mock import patch
+
+import pytest
+from gcsfs.retry import HttpError
+from google.auth._helpers import utcnow
+
+from dapla import pandas as dp
 from dapla.gcs import GCSFileSystem
 
 
@@ -5,3 +13,33 @@ def test_instance() -> None:
     # Chack that instantiation works with the current version of pyarrow
     client = GCSFileSystem()
     assert client is not None
+
+
+@pytest.mark.timeout(
+    30
+)  # Times the test out after 30 sec, this is will happen if a deadlock happens
+@patch("dapla.auth.AuthClient.is_ready")
+@patch("dapla.auth.AuthClient.fetch_google_token")
+def test_gcs_deadlock(mock_fetch_google_token, mock_is_ready) -> None:
+    # When overriding the refresh method we experienced a deadlock, resulting in the credentials never being refreshed
+    # This test checks confirms that the credentials object is updated on refresh.
+
+    mock_is_ready.return_value = True  # Mock client ready to not use ADC
+    mock_fetch_google_token.side_effect = [
+        ("FakeToken1", utcnow()),
+        ("FakeToken2", utcnow()),
+        ("FakeToken3", utcnow()),
+        ("FakeToken4", utcnow()),
+        ("FakeToken5Valid", utcnow() + timedelta(seconds=30)),
+    ]
+
+    gcs_path = "gs://ssb-dapla-pseudo-data-produkt-test/integration_tests_data/personer.parquet"
+    with pytest.raises(
+        HttpError
+    ) as exc_info:  # Since we supply invalid credentials an error should be raised
+        dp.read_pandas(gcs_path)
+    assert "Invalid Credentials" in str(exc_info.value)
+    assert (
+        mock_fetch_google_token.call_count == 5
+    )  # mock_fetch_google_token is called as part of refresh
+    # until a token that has not expired is returned
