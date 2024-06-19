b3003b2d8ecd0cfa1854da62999847ea200dd877
rpk <rupinder.kaur@ssb.no>
2024-06-19 12:57:36 +0200

    fixed nox issues

--- src/dapla/files.py
+++ src/dapla/files.py
@@ -87,7 +87,7 @@ class FileClient:
             else:
                 return list(bucket.list_blobs(prefix=file_path, soft_deleted=True))
         except google.cloud.exceptions.NotFound:
-            print("Sorry, mentioned bucket does´t exist. ")
+            print("Sorry, mentioned bucket doesn't exist.")
             return []
 
     @staticmethod
@@ -139,140 +139,137 @@ class FileClient:
                     return []
 
         except google.cloud.exceptions.NotFound:
-            print("Sorry, mentioned bucket does´t exist. ")
+            print("Sorry, mentioned bucket doesn't exist.")
 
 
-@staticmethod
-def cat(gcs_path: str) -> str:
-    """Get string content of a file from GCS.
+    @staticmethod
+    def cat(gcs_path: str) -> str:
+        """Get string content of a file from GCS.
 
-    Args:
-        gcs_path: The GCS path to a file.
+        Args:
+            gcs_path: The GCS path to a file.
 
-    Returns:
-        utf-8 decoded string content of the given file
-    """
-    return t.cast(str, FileClient.get_gcs_file_system().cat(gcs_path).decode("utf-8"))
+        Returns:
+            utf-8 decoded string content of the given file
+        """
+        return t.cast(
+            str, FileClient.get_gcs_file_system().cat(gcs_path).decode("utf-8")
+        )
 
+    @staticmethod
+    def gcs_open(
+        gcs_path: str, mode: str = "r"
+    ) -> TextIOWrapper | AbstractBufferedFile:
+        """Open a file in GCS, works like regular python open().
 
-@staticmethod
-def gcs_open(gcs_path: str, mode: str = "r") -> TextIOWrapper | AbstractBufferedFile:
-    """Open a file in GCS, works like regular python open().
+        Args:
+            gcs_path: The GCS path to a file.
+            mode: File open mode. Defaults to 'r'
 
-    Args:
-        gcs_path: The GCS path to a file.
-        mode: File open mode. Defaults to 'r'
+        Returns:
+            A file-like object.
+        """
+        return FileClient.get_gcs_file_system().open(
+            FileClient._ensure_gcs_uri_prefix(gcs_path), mode
+        )
 
-    Returns:
-        A file-like object.
-    """
-    return FileClient.get_gcs_file_system().open(
-        FileClient._ensure_gcs_uri_prefix(gcs_path), mode
-    )
+    @staticmethod
+    def load_csv_to_pandas(gcs_path: str, **kwargs: Any) -> pd.DataFrame:
+        """Reads a CSV file from Google Cloud Storage into a Pandas DataFrame.
 
+        Args:
+            gcs_path: The GCS path to a .csv file.
+            **kwargs: Additional arguments to pass to the underlying Pandas read_csv().
 
-@staticmethod
-def load_csv_to_pandas(gcs_path: str, **kwargs: Any) -> pd.DataFrame:
-    """Reads a CSV file from Google Cloud Storage into a Pandas DataFrame.
+        Returns:
+            A Pandas DataFrame.
+        """
+        return t.cast(
+            pd.DataFrame,
+            pd.read_csv(
+                FileClient._ensure_gcs_uri_prefix(gcs_path),
+                storage_options={"token": AuthClient.fetch_google_credentials()},
+                **kwargs,
+            ),
+        )
 
-    Args:
-        gcs_path: The GCS path to a .csv file.
-        **kwargs: Additional arguments to pass to the underlying Pandas read_csv().
+    @staticmethod
+    def load_json_to_pandas(gcs_path: str, **kwargs: Any) -> pd.DataFrame:
+        """Reads a JSON file from Google Cloud Storage into a Pandas DataFrame.
 
-    Returns:
-        A Pandas DataFrame.
-    """
-    return t.cast(
-        pd.DataFrame,
-        pd.read_csv(
+        Args:
+            gcs_path: The GCS path to a .json file.
+            **kwargs: Additional arguments to pass to the underlying Pandas read_json().
+
+        Returns:
+            A Pandas DataFrame.
+        """
+        return t.cast(
+            pd.DataFrame,
+            pd.read_json(
+                FileClient._ensure_gcs_uri_prefix(gcs_path),
+                storage_options={"token": AuthClient.fetch_google_credentials()},
+                **kwargs,
+            ),
+        )
+
+    @staticmethod
+    def load_xml_to_pandas(gcs_path: str, **kwargs: Any) -> pd.DataFrame:
+        """Reads an XML file from Google Cloud Storage into a Pandas DataFrame.
+
+        Args:
+            gcs_path: The GCS path to a .xml file.
+            **kwargs: Additional arguments to pass to the underlying Pandas read_xml().
+
+        Returns:
+            A Pandas DataFrame.
+        """
+        return pd.read_xml(
             FileClient._ensure_gcs_uri_prefix(gcs_path),
             storage_options={"token": AuthClient.fetch_google_credentials()},
             **kwargs,
-        ),
-    )
+        )
 
+    @staticmethod
+    def save_pandas_to_csv(df: pd.DataFrame, gcs_path: str, **kwargs: Any) -> None:
+        """Write the contents of a Pandas DataFrame to a CSV file in a bucket.
 
-@staticmethod
-def load_json_to_pandas(gcs_path: str, **kwargs: Any) -> pd.DataFrame:
-    """Reads a JSON file from Google Cloud Storage into a Pandas DataFrame.
-
-    Args:
-        gcs_path: The GCS path to a .json file.
-        **kwargs: Additional arguments to pass to the underlying Pandas read_json().
-
-    Returns:
-        A Pandas DataFrame.
-    """
-    return t.cast(
-        pd.DataFrame,
-        pd.read_json(
+        Args:
+            df: The Pandas DataFrame to save to file.
+            gcs_path: The GCS path to the destination .csv file.
+            **kwargs: Additional arguments to pass to the underlying Pandas to_csv().
+        """
+        df.to_csv(
             FileClient._ensure_gcs_uri_prefix(gcs_path),
             storage_options={"token": AuthClient.fetch_google_credentials()},
             **kwargs,
-        ),
-    )
+        )
 
+    @staticmethod
+    def save_pandas_to_json(df: pd.DataFrame, gcs_path: str, **kwargs: Any) -> None:
+        """Write the contents of a Pandas DataFrame to a JSON file in a bucket.
 
-@staticmethod
-def load_xml_to_pandas(gcs_path: str, **kwargs: Any) -> pd.DataFrame:
-    """Reads an XML file from Google Cloud Storage into a Pandas DataFrame.
+        Args:
+            df: The Pandas DataFrame to save to file.
+            gcs_path: The GCS path to the destination .json file.
+            **kwargs: Additional arguments to pass to the underlying Pandas to_json().
+        """
+        df.to_json(
+            FileClient._ensure_gcs_uri_prefix(gcs_path),
+            **kwargs,
+        )
 
-    Args:
-        gcs_path: The GCS path to a .xml file.
-        **kwargs: Additional arguments to pass to the underlying Pandas read_xml().
+    @staticmethod
+    def save_pandas_to_xml(df: pd.DataFrame, gcs_path: str, **kwargs: Any) -> None:
+        """Write the contents of a Pandas DataFrame to an XML file in a bucket.
 
-    Returns:
-        A Pandas DataFrame.
-    """
-    return pd.read_xml(
-        FileClient._ensure_gcs_uri_prefix(gcs_path),
-        storage_options={"token": AuthClient.fetch_google_credentials()},
-        **kwargs,
-    )
-
-
-@staticmethod
-def save_pandas_to_csv(df: pd.DataFrame, gcs_path: str, **kwargs: Any) -> None:
-    """Write the contents of a Pandas DataFrame to a CSV file in a bucket.
-
-    Args:
-        df: The Pandas DataFrame to save to file.
-        gcs_path: The GCS path to the destination .csv file.
-        **kwargs: Additional arguments to pass to the underlying Pandas to_csv().
-    """
-    df.to_csv(
-        FileClient._ensure_gcs_uri_prefix(gcs_path),
-        storage_options={"token": AuthClient.fetch_google_credentials()},
-        **kwargs,
-    )
-
-
-@staticmethod
-def save_pandas_to_json(df: pd.DataFrame, gcs_path: str, **kwargs: Any) -> None:
-    """Write the contents of a Pandas DataFrame to a JSON file in a bucket.
-
-    Args:
-        df: The Pandas DataFrame to save to file.
-        gcs_path: The GCS path to the destination .json file.
-        **kwargs: Additional arguments to pass to the underlying Pandas to_json().
-    """
-    df.to_json(
-        FileClient._ensure_gcs_uri_prefix(gcs_path),
-        **kwargs,
-    )
-
-
-@staticmethod
-def save_pandas_to_xml(df: pd.DataFrame, gcs_path: str, **kwargs: Any) -> None:
-    """Write the contents of a Pandas DataFrame to an XML file in a bucket.
-
-    Args:
-        df: The Pandas DataFrame to save to file.
-        gcs_path: The GCS path to the destination .xml file.
-        **kwargs: Additional arguments to pass to the underlying Pandas to_xml().
-    """
-    df.to_xml(
-        FileClient._ensure_gcs_uri_prefix(gcs_path),
-        storage_options={"token": AuthClient.fetch_google_credentials()},
-        **kwargs,
-    )
+        Args:
+            df: The Pandas DataFrame to save to file.
+            gcs_path: The GCS path to the destination .xml file.
+            **kwargs: Additional arguments to pass to the underlying Pandas to_xml().
+        """
+        df.to_xml(
+            FileClient._ensure_gcs_uri_prefix(gcs_path),
+            storage_options={"token": AuthClient.fetch_google_credentials()},
+            **kwargs,
+        )
