8cd0e1f1fc56b10e856e75c776d1d669a79c2ce7
rpk <rupinder.kaur@ssb.no>
2024-06-19 12:33:19 +0200

    added exception handling for good user experience

--- src/dapla/files.py
+++ src/dapla/files.py
@@ -2,6 +2,7 @@ import typing as t
 from io import TextIOWrapper
 from typing import Any
 
+import google
 import pandas as pd
 from fsspec.spec import AbstractBufferedFile
 from google.cloud import storage
@@ -78,13 +79,16 @@ class FileClient:
         storage_client = storage.Client(
             credentials=AuthClient.fetch_google_credentials()
         )
+        try:
+            bucket = storage_client.get_bucket(bucket_name)
 
-        bucket = storage_client.get_bucket(bucket_name)
-
-        if bucket.versioning_enabled is True:
-            return list(bucket.list_blobs(prefix=file_path, versions=True))
-        else:
-            return list(bucket.list_blobs(prefix=file_path, soft_deleted=True))
+            if bucket.versioning_enabled:
+                return list(bucket.list_blobs(prefix=file_path, versions=True))
+            else:
+                return list(bucket.list_blobs(prefix=file_path, soft_deleted=True))
+        except google.cloud.exceptions.NotFound:
+            print("Sorry, mentioned bucket does´t exist. ")
+            return []
 
     @staticmethod
     def restore_version(
@@ -93,7 +97,7 @@ class FileClient:
         source_generation_id: str,
         **kwargs: Any,
     ) -> Any:
-        """Restores deleted/non-current version of file to the live version.
+        """Restores soft deleted version of file to the live version.
 
         Args:
             source_bucket_name: source bucket name where the file is located.
@@ -107,150 +111,168 @@ class FileClient:
         storage_client = storage.Client(
             credentials=AuthClient.fetch_google_credentials()
         )
-        source_bucket = storage_client.get_bucket(source_bucket_name)
-        source_file = source_bucket.blob(source_file_name)
 
-        if source_bucket.versioning_enabled is True:
-            return source_bucket.copy_blob(
-                blob=source_file,
-                destination_bucket=source_bucket,
-                source_generation=source_generation_id,
-                **kwargs,
-            )
+        try:
+            source_bucket = storage_client.get_bucket(source_bucket_name)
+            source_file = source_bucket.blob(source_file_name)
 
-        else:
-            return source_bucket.restore_blob(
-                blob_name=source_file_name, generation=source_generation_id, **kwargs
-            )
+            if source_bucket.versioning_enabled:
+                try:
+                    return source_bucket.copy_blob(
+                        blob=source_file,
+                        destination_bucket=source_bucket,
+                        source_generation=source_generation_id,
+                        **kwargs,
+                    )
+                except google.cloud.exceptions.NotFound as e:
+                    print("No such object. Check file name or the generation number", e)
+                    return []
+            else:
+                try:
+                    return source_bucket.restore_blob(
+                        blob_name=source_file_name,
+                        generation=source_generation_id,
+                        **kwargs,
+                    )
+                except google.cloud.exceptions.NotFound as e:
+                    print("No such object. Check file name or the generation number", e)
+                    return []
 
-    @staticmethod
-    def cat(gcs_path: str) -> str:
-        """Get string content of a file from GCS.
+        except google.cloud.exceptions.NotFound:
+            print("Sorry, mentioned bucket does´t exist. ")
 
-        Args:
-            gcs_path: The GCS path to a file.
 
-        Returns:
-            utf-8 decoded string content of the given file
-        """
-        return t.cast(
-            str, FileClient.get_gcs_file_system().cat(gcs_path).decode("utf-8")
-        )
+@staticmethod
+def cat(gcs_path: str) -> str:
+    """Get string content of a file from GCS.
 
-    @staticmethod
-    def gcs_open(
-        gcs_path: str, mode: str = "r"
-    ) -> TextIOWrapper | AbstractBufferedFile:
-        """Open a file in GCS, works like regular python open().
+    Args:
+        gcs_path: The GCS path to a file.
 
-        Args:
-            gcs_path: The GCS path to a file.
-            mode: File open mode. Defaults to 'r'
+    Returns:
+        utf-8 decoded string content of the given file
+    """
+    return t.cast(str, FileClient.get_gcs_file_system().cat(gcs_path).decode("utf-8"))
 
-        Returns:
-            A file-like object.
-        """
-        return FileClient.get_gcs_file_system().open(
-            FileClient._ensure_gcs_uri_prefix(gcs_path), mode
-        )
 
-    @staticmethod
-    def load_csv_to_pandas(gcs_path: str, **kwargs: Any) -> pd.DataFrame:
-        """Reads a CSV file from Google Cloud Storage into a Pandas DataFrame.
+@staticmethod
+def gcs_open(gcs_path: str, mode: str = "r") -> TextIOWrapper | AbstractBufferedFile:
+    """Open a file in GCS, works like regular python open().
 
-        Args:
-            gcs_path: The GCS path to a .csv file.
-            **kwargs: Additional arguments to pass to the underlying Pandas read_csv().
+    Args:
+        gcs_path: The GCS path to a file.
+        mode: File open mode. Defaults to 'r'
 
-        Returns:
-            A Pandas DataFrame.
-        """
-        return t.cast(
-            pd.DataFrame,
-            pd.read_csv(
-                FileClient._ensure_gcs_uri_prefix(gcs_path),
-                storage_options={"token": AuthClient.fetch_google_credentials()},
-                **kwargs,
-            ),
-        )
+    Returns:
+        A file-like object.
+    """
+    return FileClient.get_gcs_file_system().open(
+        FileClient._ensure_gcs_uri_prefix(gcs_path), mode
+    )
 
-    @staticmethod
-    def load_json_to_pandas(gcs_path: str, **kwargs: Any) -> pd.DataFrame:
-        """Reads a JSON file from Google Cloud Storage into a Pandas DataFrame.
 
-        Args:
-            gcs_path: The GCS path to a .json file.
-            **kwargs: Additional arguments to pass to the underlying Pandas read_json().
+@staticmethod
+def load_csv_to_pandas(gcs_path: str, **kwargs: Any) -> pd.DataFrame:
+    """Reads a CSV file from Google Cloud Storage into a Pandas DataFrame.
 
-        Returns:
-            A Pandas DataFrame.
-        """
-        return t.cast(
-            pd.DataFrame,
-            pd.read_json(
-                FileClient._ensure_gcs_uri_prefix(gcs_path),
-                storage_options={"token": AuthClient.fetch_google_credentials()},
-                **kwargs,
-            ),
-        )
+    Args:
+        gcs_path: The GCS path to a .csv file.
+        **kwargs: Additional arguments to pass to the underlying Pandas read_csv().
 
-    @staticmethod
-    def load_xml_to_pandas(gcs_path: str, **kwargs: Any) -> pd.DataFrame:
-        """Reads an XML file from Google Cloud Storage into a Pandas DataFrame.
-
-        Args:
-            gcs_path: The GCS path to a .xml file.
-            **kwargs: Additional arguments to pass to the underlying Pandas read_xml().
-
-        Returns:
-            A Pandas DataFrame.
-        """
-        return pd.read_xml(
+    Returns:
+        A Pandas DataFrame.
+    """
+    return t.cast(
+        pd.DataFrame,
+        pd.read_csv(
             FileClient._ensure_gcs_uri_prefix(gcs_path),
             storage_options={"token": AuthClient.fetch_google_credentials()},
             **kwargs,
-        )
+        ),
+    )
 
-    @staticmethod
-    def save_pandas_to_csv(df: pd.DataFrame, gcs_path: str, **kwargs: Any) -> None:
-        """Write the contents of a Pandas DataFrame to a CSV file in a bucket.
 
-        Args:
-            df: The Pandas DataFrame to save to file.
-            gcs_path: The GCS path to the destination .csv file.
-            **kwargs: Additional arguments to pass to the underlying Pandas to_csv().
-        """
-        df.to_csv(
+@staticmethod
+def load_json_to_pandas(gcs_path: str, **kwargs: Any) -> pd.DataFrame:
+    """Reads a JSON file from Google Cloud Storage into a Pandas DataFrame.
+
+    Args:
+        gcs_path: The GCS path to a .json file.
+        **kwargs: Additional arguments to pass to the underlying Pandas read_json().
+
+    Returns:
+        A Pandas DataFrame.
+    """
+    return t.cast(
+        pd.DataFrame,
+        pd.read_json(
             FileClient._ensure_gcs_uri_prefix(gcs_path),
             storage_options={"token": AuthClient.fetch_google_credentials()},
             **kwargs,
-        )
+        ),
+    )
 
-    @staticmethod
-    def save_pandas_to_json(df: pd.DataFrame, gcs_path: str, **kwargs: Any) -> None:
-        """Write the contents of a Pandas DataFrame to a JSON file in a bucket.
 
-        Args:
-            df: The Pandas DataFrame to save to file.
-            gcs_path: The GCS path to the destination .json file.
-            **kwargs: Additional arguments to pass to the underlying Pandas to_json().
-        """
-        df.to_json(
-            FileClient._ensure_gcs_uri_prefix(gcs_path),
-            **kwargs,
-        )
+@staticmethod
+def load_xml_to_pandas(gcs_path: str, **kwargs: Any) -> pd.DataFrame:
+    """Reads an XML file from Google Cloud Storage into a Pandas DataFrame.
 
-    @staticmethod
-    def save_pandas_to_xml(df: pd.DataFrame, gcs_path: str, **kwargs: Any) -> None:
-        """Write the contents of a Pandas DataFrame to an XML file in a bucket.
+    Args:
+        gcs_path: The GCS path to a .xml file.
+        **kwargs: Additional arguments to pass to the underlying Pandas read_xml().
 
-        Args:
-            df: The Pandas DataFrame to save to file.
-            gcs_path: The GCS path to the destination .xml file.
-            **kwargs: Additional arguments to pass to the underlying Pandas to_xml().
-        """
-        df.to_xml(
-            FileClient._ensure_gcs_uri_prefix(gcs_path),
-            storage_options={"token": AuthClient.fetch_google_credentials()},
-            **kwargs,
-        )
+    Returns:
+        A Pandas DataFrame.
+    """
+    return pd.read_xml(
+        FileClient._ensure_gcs_uri_prefix(gcs_path),
+        storage_options={"token": AuthClient.fetch_google_credentials()},
+        **kwargs,
+    )
+
+
+@staticmethod
+def save_pandas_to_csv(df: pd.DataFrame, gcs_path: str, **kwargs: Any) -> None:
+    """Write the contents of a Pandas DataFrame to a CSV file in a bucket.
+
+    Args:
+        df: The Pandas DataFrame to save to file.
+        gcs_path: The GCS path to the destination .csv file.
+        **kwargs: Additional arguments to pass to the underlying Pandas to_csv().
+    """
+    df.to_csv(
+        FileClient._ensure_gcs_uri_prefix(gcs_path),
+        storage_options={"token": AuthClient.fetch_google_credentials()},
+        **kwargs,
+    )
+
+
+@staticmethod
+def save_pandas_to_json(df: pd.DataFrame, gcs_path: str, **kwargs: Any) -> None:
+    """Write the contents of a Pandas DataFrame to a JSON file in a bucket.
+
+    Args:
+        df: The Pandas DataFrame to save to file.
+        gcs_path: The GCS path to the destination .json file.
+        **kwargs: Additional arguments to pass to the underlying Pandas to_json().
+    """
+    df.to_json(
+        FileClient._ensure_gcs_uri_prefix(gcs_path),
+        **kwargs,
+    )
+
+
+@staticmethod
+def save_pandas_to_xml(df: pd.DataFrame, gcs_path: str, **kwargs: Any) -> None:
+    """Write the contents of a Pandas DataFrame to an XML file in a bucket.
+
+    Args:
+        df: The Pandas DataFrame to save to file.
+        gcs_path: The GCS path to the destination .xml file.
+        **kwargs: Additional arguments to pass to the underlying Pandas to_xml().
+    """
+    df.to_xml(
+        FileClient._ensure_gcs_uri_prefix(gcs_path),
+        storage_options={"token": AuthClient.fetch_google_credentials()},
+        **kwargs,
+    )
--- tests/test_files.py
+++ tests/test_files.py
@@ -3,6 +3,8 @@ import unittest
 from unittest.mock import Mock
 from unittest.mock import patch
 
+import google
+
 from dapla import FileClient
 
 PATH_WITH_PREFIX = "gs://bucket/path"
@@ -28,13 +30,13 @@ class TestFiles(unittest.TestCase):
 
     @patch("dapla.auth.AuthClient.fetch_google_credentials", return_value="credentials")
     @patch("google.cloud.storage.Client")
-    def test_get_valid_versions_versioning_enabled(
+    def test_get_versions_valid(
         self, mock_client: Mock, mock_auth_client: Mock
     ) -> None:
+        # Arrange
         bucket_name = "test-bucket"
         file_name = "test-file.txt"
         mock_bucket = Mock()
-        mock_bucket.versioning_enabled = True
         mock_client.return_value.get_bucket.return_value = mock_bucket
         mock_blob1 = Mock(
             name="test-file.txt",
@@ -46,7 +48,7 @@ class TestFiles(unittest.TestCase):
             name="test-file.txt",
             generation=2,
             updated="2023-04-02T00:00:00Z",
-            time_deleted="2023-04-03T00:00:00Z",
+            time_deleted=None,
         )
         mock_bucket.list_blobs.return_value = [mock_blob1, mock_blob2]
 
@@ -62,61 +64,14 @@ class TestFiles(unittest.TestCase):
         assert files[0].updated == mock_blob1.updated
         assert files[0].time_deleted is None
 
-        assert files[1].name == mock_blob2.name
-        assert files[1].generation == mock_blob2.generation
-        assert files[1].updated == mock_blob2.updated
-        assert files[1].time_deleted == "2023-04-03T00:00:00Z"
-
-    @patch("dapla.auth.AuthClient.fetch_google_credentials", return_value="credentials")
-    @patch("google.cloud.storage.Client")
-    def test_get_valid_versions_versioning_disabled(
-        self, mock_client: Mock, mock_auth_client: Mock
-    ) -> None:
-        bucket_name = "test-bucket"
-        file_name = "test-file.txt"
-        mock_bucket = Mock()
-        mock_bucket.versioning_enabled = False
-        mock_client.return_value.get_bucket.return_value = mock_bucket
-        mock_blob1 = Mock(
-            name="test-file.txt",
-            generation=1,
-            updated="2023-04-01T00:00:00Z",
-            time_deleted=None,
-        )
-        mock_blob2 = Mock(
-            name="test-file.txt",
-            generation=2,
-            updated="2023-04-02T00:00:00Z",
-            time_deleted="2023-04-03T00:00:00Z",
-        )
-        mock_bucket.list_blobs.return_value = [mock_blob1, mock_blob2]
-
-        files = FileClient.get_versions(bucket_name, file_name)
-
-        mock_client.return_value.get_bucket.assert_called_with(bucket_name)
-        mock_bucket.list_blobs.assert_called_with(prefix=file_name, soft_deleted=True)
-
-        assert len(files) == 2
-
-        assert files[0].name == mock_blob1.name
-        assert files[0].generation == mock_blob1.generation
-        assert files[0].updated == mock_blob1.updated
-        assert files[0].time_deleted is None
-
-        assert files[1].name == mock_blob2.name
-        assert files[1].generation == mock_blob2.generation
-        assert files[1].updated == mock_blob2.updated
-        assert files[1].time_deleted == "2023-04-03T00:00:00Z"
-
     @patch("dapla.auth.AuthClient.fetch_google_credentials", return_value="credentials")
     @patch("google.cloud.storage.Client")
     def test_get_versions_nonexistent_file(
         self, mock_client: Mock, mock_auth_client: Mock
     ) -> None:
         bucket_name = "test-bucket"
-        file_name = "../../../invalid/file/path"
+        file_name = "nonexistent-file.txt"
         mock_bucket = Mock()
-        mock_bucket.versioning_enabled = True
         mock_client.return_value.get_bucket.return_value = mock_bucket
         mock_bucket.list_blobs.return_value = []
 
@@ -130,17 +85,129 @@ class TestFiles(unittest.TestCase):
 
     @patch("dapla.auth.AuthClient.fetch_google_credentials", return_value="credentials")
     @patch("google.cloud.storage.Client")
-    def test_restore_version_versioning_enabled(
+    def test_get_versions_empty_bucket(
+        self, mock_client: Mock, mock_auth_client: Mock
+    ) -> None:
+        bucket_name = "test-bucket"
+        file_name = "test-file.txt"
+        mock_bucket = Mock()
+        mock_client.return_value.get_bucket.return_value = mock_bucket
+        mock_bucket.list_blobs.return_value = []
+
+        files = FileClient.get_versions(bucket_name, file_name)
+
+        mock_client.return_value.get_bucket.assert_called_with(bucket_name)
+        mock_bucket.list_blobs.assert_called_with(prefix=file_name, versions=True)
+
+        assert len(files) == 0
+        assert files == []
+
+    @patch("dapla.auth.AuthClient.fetch_google_credentials", return_value="credentials")
+    @patch("google.cloud.storage.Client")
+    def test_restore_version_success(
+        self, mock_client: Mock, mock_auth_client: Mock
+    ) -> None:
+        mock_bucket = Mock()
+        mock_source_blob = Mock()
+        mock_client.return_value.get_bucket.return_value = mock_bucket
+        mock_bucket.blob.return_value = mock_source_blob
+
+        blob = FileClient.restore_version(
+            source_bucket_name="test-bucket",
+            source_file_name="test-file.txt",
+            source_generation_id="1234567890",
+            new_name="restored-file.txt",
+            if_generation_match="0",
+        )
+
+        mock_client.return_value.get_bucket.assert_called_with("test-bucket")
+        mock_bucket.blob.assert_called_with("test-file.txt")
+        mock_bucket.copy_blob.assert_called_with(
+            blob=mock_source_blob,
+            destination_bucket=mock_bucket,
+            source_generation="1234567890",
+            new_name="restored-file.txt",
+            if_generation_match="0",
+        )
+        assert blob == mock_bucket.copy_blob.return_value
+
+    @patch("dapla.auth.AuthClient.fetch_google_credentials", return_value="credentials")
+    @patch("google.cloud.storage.Client")
+    def test_restore_version_existing_live_version(
+        self, mock_client: Mock, mock_auth_client: Mock
+    ) -> None:
+        mock_bucket = Mock()
+        mock_source_blob = Mock()
+        mock_client.return_value.get_bucket.return_value = mock_bucket
+        mock_bucket.versioning_enabled = True
+        mock_bucket.blob.return_value = mock_source_blob
+
+        blob = FileClient.restore_version(
+            source_bucket_name="test-bucket",
+            source_file_name="test-file.txt",
+            source_generation_id="1234567890",
+            if_generation_match="0987654321",
+        )
+
+        mock_client.return_value.get_bucket.assert_called_with("test-bucket")
+        mock_bucket.blob.assert_called_with("test-file.txt")
+        mock_bucket.copy_blob.assert_called_with(
+            blob=mock_source_blob,
+            destination_bucket=mock_bucket,
+            source_generation="1234567890",
+            if_generation_match="0987654321",
+        )
+        assert blob == mock_bucket.copy_blob.return_value
+
+    @patch("dapla.auth.AuthClient.fetch_google_credentials", return_value="credentials")
+    @patch("google.cloud.storage.Client")
+    def test_get_versions_with_invalid_bucket_name(
+        self, mock_client: Mock, mock_auth_client: Mock
+    ) -> None:
+        bucket_name = "invalid-bucket"
+        file_name = "test-file.txt"
+        mock_client.return_value.get_bucket.side_effect = (
+            google.cloud.exceptions.NotFound("Sorry, mentioned bucket does´t exist. ")
+        )
+
+        files = FileClient.get_versions(bucket_name, file_name)
+
+        mock_client.return_value.get_bucket.assert_called_with(bucket_name)
+        assert len(files) == 0
+
+    @patch("dapla.auth.AuthClient.fetch_google_credentials", return_value="credentials")
+    @patch("google.cloud.storage.Client")
+    def test_restore_version_with_invalid_bucket_name(
+        self, mock_client: Mock, mock_auth_client: Mock
+    ) -> None:
+        source_bucket_name = "invalid-bucket"
+        source_file_name = "test-file.txt"
+        source_generation_id = "1234567890"
+        mock_client.return_value.get_bucket.side_effect = (
+            google.cloud.exceptions.NotFound("Sorry, mentioned bucket does´t exist. ")
+        )
+
+        result = FileClient.restore_version(
+            source_bucket_name, source_file_name, source_generation_id
+        )
+
+        mock_client.return_value.get_bucket.assert_called_with(source_bucket_name)
+        assert result is None
+
+    @patch("dapla.auth.AuthClient.fetch_google_credentials", return_value="credentials")
+    @patch("google.cloud.storage.Client")
+    def test_restore_version_with_invalid_file_name(
         self, mock_client: Mock, mock_auth_client: Mock
     ) -> None:
         source_bucket_name = "test-bucket"
-        source_file_name = "test-file.txt"
+        source_file_name = "invalid-file.txt"
         source_generation_id = "1234567890"
         mock_bucket = Mock()
         mock_bucket.versioning_enabled = True
         mock_client.return_value.get_bucket.return_value = mock_bucket
-        mock_blob = Mock()
-        mock_bucket.copy_blob.return_value = mock_blob
+        mock_bucket.copy_blob.side_effect = google.cloud.exceptions.NotFound(
+            "No such object. Check file name or the generation number"
+        )
 
         result = FileClient.restore_version(
             source_bucket_name, source_file_name, source_generation_id
@@ -152,63 +219,11 @@ class TestFiles(unittest.TestCase):
             destination_bucket=mock_bucket,
             source_generation=source_generation_id,
         )
-        assert result == mock_blob
+        assert result == []
 
     @patch("dapla.auth.AuthClient.fetch_google_credentials", return_value="credentials")
     @patch("google.cloud.storage.Client")
-    def test_restore_version_versioning_disabled(
-        self, mock_client: Mock, mock_auth_client: Mock
-    ) -> None:
-        source_bucket_name = "test-bucket"
-        source_file_name = "test-file.txt"
-        source_generation_id = "1234567890"
-        mock_bucket = Mock()
-        mock_bucket.versioning_enabled = False
-        mock_client.return_value.get_bucket.return_value = mock_bucket
-        mock_blob = Mock()
-        mock_bucket.restore_blob.return_value = mock_blob
-
-        result = FileClient.restore_version(
-            source_bucket_name, source_file_name, source_generation_id
-        )
-
-        mock_client.return_value.get_bucket.assert_called_with(source_bucket_name)
-        mock_bucket.restore_blob.assert_called_with(
-            blob_name=source_file_name, generation=source_generation_id
-        )
-        assert result == mock_blob
-
-    @patch("dapla.auth.AuthClient.fetch_google_credentials", return_value="credentials")
-    @patch("google.cloud.storage.Client")
-    def test_restore_version_with_kwargs(
-        self, mock_client: Mock, mock_auth_client: Mock
-    ) -> None:
-        source_bucket_name = "test-bucket"
-        source_file_name = "test-file.txt"
-        source_generation_id = "1234567890"
-        mock_bucket = Mock()
-        mock_bucket.versioning_enabled = True
-        mock_client.return_value.get_bucket.return_value = mock_bucket
-        mock_blob = Mock()
-        mock_bucket.copy_blob.return_value = mock_blob
-        kwargs = {"metadata": {"key": "value"}}
-
-        result = FileClient.restore_version(
-            source_bucket_name, source_file_name, source_generation_id, **kwargs
-        )
-
-        mock_client.return_value.get_bucket.assert_called_with(source_bucket_name)
-        mock_bucket.copy_blob.assert_called_with(
-            blob=mock_bucket.blob(source_file_name),
-            destination_bucket=mock_bucket,
-            source_generation=source_generation_id,
-            **kwargs,
-        )
-        assert result == mock_blob
-
-    @patch("dapla.auth.AuthClient.fetch_google_credentials", return_value="credentials")
-    @patch("google.cloud.storage.Client")
-    def test_restore_version_invalid_generation_id(
+    def test_restore_version_with_invalid_generation_id(
         self, mock_client: Mock, mock_auth_client: Mock
     ) -> None:
         source_bucket_name = "test-bucket"
