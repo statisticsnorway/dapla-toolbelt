f9b440185d4312e5543e32adae2983ff5e974c3a
Michael Moen Allport <mmallport@gmail.com>
2024-11-28 12:51:42 +0100

    ree more envvars

--- tests/test_backports.py
+++ tests/test_backports.py
@@ -1,22 +1,20 @@
 import os
-from typing import Any
 from unittest import mock
 from unittest.mock import Mock
-
-import pytest
 
 from dapla.backports import show
 from dapla.gcs import GCSFileSystem
 
 
-@pytest.fixture(scope="function")
-def mock_dapla_region(scope: str = "class") -> Any:
-    with mock.patch.dict(os.environ, {"DAPLA_REGION": "DAPLA_LAB"}):
-        yield
-
-
+@mock.patch.dict(
+    "dapla.auth.os.environ",
+    {
+        "DAPLA_REGION": "DAPLA_LAB",
+    },
+    clear=True,
+)
 @mock.patch("dapla.backports.FileClient")
-def test_show_all_subfolders(file_client_mock: Mock, mock_dapla_region: Mock) -> None:
+def test_show_all_subfolders(file_client_mock: Mock) -> None:
     assert os.getenv("DAPLA_REGION") == "DAPLA_LAB"
     file_client_mock.get_gcs_file_system.return_value = GCSFileSystem()
     result = show("gs://anaconda-public-data/nyc-taxi/")
@@ -31,16 +29,30 @@ def test_show_all_subfolders(file_client_mock: Mock, mock_dapla_region: Mock) ->
     ]
 
 
+@mock.patch.dict(
+    "dapla.auth.os.environ",
+    {
+        "DAPLA_REGION": "DAPLA_LAB",
+    },
+    clear=True,
+)
 @mock.patch("dapla.backports.FileClient")
-def test_show_leaf_folder(file_client_mock: Mock, mock_dapla_region: Mock) -> None:
+def test_show_leaf_folder(file_client_mock: Mock) -> None:
     assert os.getenv("DAPLA_REGION") == "DAPLA_LAB"
     file_client_mock.get_gcs_file_system.return_value = GCSFileSystem()
     result = show("gs://anaconda-public-data/nyc-taxi/csv/2014")
     assert result == ["/nyc-taxi/csv/2014"]
 
 
+@mock.patch.dict(
+    "dapla.auth.os.environ",
+    {
+        "DAPLA_REGION": "DAPLA_LAB",
+    },
+    clear=True,
+)
 @mock.patch("dapla.backports.FileClient")
-def test_show_invalid_folder(file_client_mock: Mock, mock_dapla_region: Mock) -> None:
+def test_show_invalid_folder(file_client_mock: Mock) -> None:
     assert os.getenv("DAPLA_REGION") == "DAPLA_LAB"
     file_client_mock.get_gcs_file_system.return_value = GCSFileSystem()
     result = show("gs://anaconda-public-data/nyc-taxi/unknown")
