ea6859a7f4e70bd8f39ddf476963a78f4da9d64b
Michael Moen Allport <mmallport@gmail.com>
2024-11-28 12:37:43 +0100

    ree env vars

--- tests/test_backports.py
+++ tests/test_backports.py
@@ -1,14 +1,23 @@
 import os
+from typing import Any
 from unittest import mock
 from unittest.mock import Mock
+
+import pytest
 
 from dapla.backports import show
 from dapla.gcs import GCSFileSystem
 
 
+@pytest.fixture(scope="function")
+def mock_dapla_region(scope: str = "class") -> Any:
+    with mock.patch.dict(os.environ, {"DAPLA_REGION": "DAPLA_LAB"}):
+        yield
+
+
 @mock.patch("dapla.backports.FileClient")
-def test_show_all_subfolders(file_client_mock: Mock) -> None:
-    os.environ.update({"DAPLA_REGION": "DAPLA_LAB"})
+def test_show_all_subfolders(file_client_mock: Mock, mock_dapla_region: Mock) -> None:
+    assert os.getenv("DAPLA_REGION") == "DAPLA_LAB"
     file_client_mock.get_gcs_file_system.return_value = GCSFileSystem()
     result = show("gs://anaconda-public-data/nyc-taxi/")
     assert result == [
@@ -23,16 +32,16 @@ def test_show_all_subfolders(file_client_mock: Mock) -> None:
 
 
 @mock.patch("dapla.backports.FileClient")
-def test_show_leaf_folder(file_client_mock: Mock) -> None:
-    os.environ.update({"DAPLA_REGION": "DAPLA_LAB"})
+def test_show_leaf_folder(file_client_mock: Mock, mock_dapla_region: Mock) -> None:
+    assert os.getenv("DAPLA_REGION") == "DAPLA_LAB"
     file_client_mock.get_gcs_file_system.return_value = GCSFileSystem()
     result = show("gs://anaconda-public-data/nyc-taxi/csv/2014")
     assert result == ["/nyc-taxi/csv/2014"]
 
 
 @mock.patch("dapla.backports.FileClient")
-def test_show_invalid_folder(file_client_mock: Mock) -> None:
-    os.environ.update({"DAPLA_REGION": "DAPLA_LAB"})
+def test_show_invalid_folder(file_client_mock: Mock, mock_dapla_region: Mock) -> None:
+    assert os.getenv("DAPLA_REGION") == "DAPLA_LAB"
     file_client_mock.get_gcs_file_system.return_value = GCSFileSystem()
     result = show("gs://anaconda-public-data/nyc-taxi/unknown")
     assert result == []
