7b408ac23c24ed33a397003ed7bacf81d9261b38
Michael Moen Allport <mmallport@gmail.com>
2024-11-28 12:26:06 +0100

    set envs in different manner

--- tests/test_backports.py
+++ tests/test_backports.py
@@ -8,7 +8,7 @@ from dapla.gcs import GCSFileSystem
 
 @mock.patch("dapla.backports.FileClient")
 def test_show_all_subfolders(file_client_mock: Mock) -> None:
-    os.environ["DAPLA_REGION"] = "DAPLA_LAB"
+    os.environ.update({"DAPLA_REGION": "DAPLA_LAB"})
     file_client_mock.get_gcs_file_system.return_value = GCSFileSystem()
     result = show("gs://anaconda-public-data/nyc-taxi/")
     assert result == [
@@ -24,7 +24,7 @@ def test_show_all_subfolders(file_client_mock: Mock) -> None:
 
 @mock.patch("dapla.backports.FileClient")
 def test_show_leaf_folder(file_client_mock: Mock) -> None:
-    os.environ["DAPLA_REGION"] = "DAPLA_LAB"
+    os.environ.update({"DAPLA_REGION": "DAPLA_LAB"})
     file_client_mock.get_gcs_file_system.return_value = GCSFileSystem()
     result = show("gs://anaconda-public-data/nyc-taxi/csv/2014")
     assert result == ["/nyc-taxi/csv/2014"]
@@ -32,7 +32,7 @@ def test_show_leaf_folder(file_client_mock: Mock) -> None:
 
 @mock.patch("dapla.backports.FileClient")
 def test_show_invalid_folder(file_client_mock: Mock) -> None:
-    os.environ["DAPLA_REGION"] = "DAPLA_LAB"
+    os.environ.update({"DAPLA_REGION": "DAPLA_LAB"})
     file_client_mock.get_gcs_file_system.return_value = GCSFileSystem()
     result = show("gs://anaconda-public-data/nyc-taxi/unknown")
     assert result == []
