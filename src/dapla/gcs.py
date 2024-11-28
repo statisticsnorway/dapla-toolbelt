579e3ec9bf525f3cb1314846c5568e0984468762
Michael Moen Allport <mmallport@gmail.com>
2024-11-28 10:47:29 +0100

    Move logic into class

--- src/dapla/gcs.py
+++ src/dapla/gcs.py
@@ -1,29 +1,26 @@
 import os
 import typing as t
 from typing import Any
-from typing import Optional
 
 import gcsfs
-from google.oauth2.credentials import Credentials
 
+from dapla.auth import AuthClient
 from dapla.const import DaplaRegion
 
 
 class GCSFileSystem(gcsfs.GCSFileSystem):  # type: ignore [misc]
     """GCSFileSystem is a wrapper around gcsfs.GCSFileSystem."""
 
-    def __init__(
-        self, token: Optional[dict[str, str] | str | Credentials] = None, **kwargs: Any
-    ) -> None:
+    def __init__(self, **kwargs: Any) -> None:
         """Initialize GCSFileSystem."""
         if os.getenv("DAPLA_REGION") == str(DaplaRegion.DAPLA_LAB) or os.getenv(
             "DAPLA_REGION"
         ) == str(DaplaRegion.CLOUD_RUN):
             # When using environments with ADC, return a GCSFS using auth
             # from the environment
-            super.__init__(**kwargs)
-
-        super().__init__(token=token, **kwargs)
+            super().__init__(**kwargs)
+        else:
+            super().__init__(token=AuthClient.fetch_google_credentials(), **kwargs)
 
     def isdir(self, path: str) -> bool:
         """Check if path is a directory."""
