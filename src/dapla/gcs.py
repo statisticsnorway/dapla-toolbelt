49a9826d231c9b6decf35e2f4173605f22ecfd83
Michael Moen Allport <mmallport@gmail.com>
2024-11-28 10:54:57 +0100

    Fix enum string cast

--- src/dapla/gcs.py
+++ src/dapla/gcs.py
@@ -13,9 +13,10 @@ class GCSFileSystem(gcsfs.GCSFileSystem):  # type: ignore [misc]
 
     def __init__(self, **kwargs: Any) -> None:
         """Initialize GCSFileSystem."""
-        if os.getenv("DAPLA_REGION") == str(DaplaRegion.DAPLA_LAB) or os.getenv(
-            "DAPLA_REGION"
-        ) == str(DaplaRegion.CLOUD_RUN):
+        if (
+            os.getenv("DAPLA_REGION") == DaplaRegion.DAPLA_LAB.value
+            or os.getenv("DAPLA_REGION") == DaplaRegion.CLOUD_RUN.value
+        ):
             # When using environments with ADC, return a GCSFS using auth
             # from the environment
             super().__init__(**kwargs)
