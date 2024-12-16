3e6b4e6ced53f0311da4c5836a0d30cf82e23039
Rupinder Kaur <Rupinder.Kaur@ssb.no>
2024-12-16 13:39:49 +0100

    Get guardian url based on dapla environment (#189)
    * Added guardian url for test and prod env

--- tests/test_gcs.py
+++ tests/test_gcs.py
@@ -1,7 +1,38 @@
+import os
+from unittest.mock import MagicMock
+from unittest.mock import patch
+
 from dapla.gcs import GCSFileSystem
 
 
-def test_instance() -> None:
-    # Chack that instantiation works with the current version of pyarrow
+@patch("google.auth.default", return_value=(None, None))
+def test_instance(mock_auth: MagicMock) -> None:
     client = GCSFileSystem()
+    assert client is not None
+
+
+@patch("google.auth.default", return_value=(None, None))
+def test_init_with_dapla_lab(mock_auth: MagicMock) -> None:
+    os.environ["DAPLA_REGION"] = "DAPLA_LABx"
+    client = GCSFileSystem(project="test-project")
+    assert client is not None
+
+
+@patch("google.auth.default", return_value=(None, None))
+def test_init_with_cloud_run(mock_auth: MagicMock) -> None:
+    os.environ["DAPLA_REGION"] = "CLOUD_RUN"
+    client = GCSFileSystem(project="test-project")
+    assert client is not None
+
+
+@patch("google.auth.default", return_value=(None, None))
+def test_init_with_custom_region(mock_auth: MagicMock) -> None:
+    os.environ["DAPLA_REGION"] = "custom-region"
+    client = GCSFileSystem(project="test-project")
+    assert client is not None
+
+
+@patch("google.auth.default", return_value=(None, None))
+def test_init_with_additional_kwargs(mock_auth: MagicMock) -> None:
+    client = GCSFileSystem(project="test-project", timeout=100)
     assert client is not None
